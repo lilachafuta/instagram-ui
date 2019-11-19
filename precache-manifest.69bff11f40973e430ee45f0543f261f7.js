079b4ea78423e03c0039bb2eb386f87b3c6b8594
Lilach Afuta <lilachafuta92@gmail.com>
2019-11-19 18:59:56 +0200

    Updates

---
+++ precache-manifest.69bff11f40973e430ee45f0543f261f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762a1c9d5db0b33c93429acbe1455e51",
+    "url": "/instagram-ui/index.html"
+  },
+  {
+    "revision": "f9134ae9ccda1232be23",
+    "url": "/instagram-ui/static/css/main.c04d27fb.chunk.css"
+  },
+  {
+    "revision": "304b9214ee6bb006d125",
+    "url": "/instagram-ui/static/js/2.ff66cc05.chunk.js"
+  },
+  {
+    "revision": "f9134ae9ccda1232be23",
+    "url": "/instagram-ui/static/js/main.e29218e2.chunk.js"
+  },
+  {
+    "revision": "fd329e52972bd349eb48",
+    "url": "/instagram-ui/static/js/runtime-main.9b17314b.js"
+  }
+]);

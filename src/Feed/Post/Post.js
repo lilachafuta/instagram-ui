40d3b69118f8db0e0ec0d9c572a97d3674f55e97
Lilach Afuta <lilachafuta92@gmail.com>
2019-11-19 22:14:56 +0200

    changing time icon and body card

--- src/Feed/Post/Post.js
+++ src/Feed/Post/Post.js
@@ -4,7 +4,7 @@ import Moment from 'react-moment';
 import TagList from "./TagList/TagList";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle, faClock } from '@fortawesome/free-solid-svg-icons'
 
 class Post extends Component {
     render() {
@@ -12,16 +12,17 @@ class Post extends Component {
             <div className="Post">
                 <div className="card" style={{width: '400px',height: '550px'}}>
                     <header className="header">
-                        <div><FontAwesomeIcon icon={faUserCircle} className="icon-user"/> </div>
+                        <div><FontAwesomeIcon icon={faUserCircle} className="icon-user"/></div>
                     </header>
                     <img src={this.props.url} className="card-img-top" alt="..."/>
+                    <p className="likes">{this.props.likes} <FontAwesomeIcon icon={faHeart} className="icon"/></p>
                         <div className="card-body">
-                            <p className="card-text">{this.props.likes} <FontAwesomeIcon icon={faHeart} className="icon"/></p>
                             <p className="card-text-title">{this.props.title}</p>
                             <TagList tags={this.props.tags} />
-                            <p className="card-text"><small className="text-muted">
-                                Last updated at <Moment format="HH:MM - MMM DD" unix >{this.props.time}</Moment>
-                            </small></p>
+                            <small className="text-muted">
+                                Last updated <FontAwesomeIcon icon={faClock} className="icon-time"/>
+                                <Moment format=" HH:MM - MMM DD" unix >{this.props.time}</Moment>
+                            </small>
                         </div>
                 </div>
             </div>
